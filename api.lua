85e0ba0ae1398e78d195781883872a5219b0b3b5
Jordan Irwin <antumdeluge@gmail.com>
2021-04-28 19:51:13 -0700

    Make nodes colorable if unifieddyes available

--- api.lua
+++ api.lua
@@ -46,7 +46,7 @@ local box = {
 --  @param name
 --  @param def
 function trampoline.register_tramp(name, def)
-	core.register_node("trampoline:" .. name, {
+	local new_def = {
 		description = def.description,
 		drawtype = "nodebox",
 		node_box = box,
@@ -65,5 +65,14 @@ function trampoline.register_tramp(name, def)
 		sounds = {
 			footstep = "trampoline_boing",
 		},
-	})
+	}
+
+	if core.global_exists("unifieddyes") then
+		new_def.paramtype2 = "color"
+		new_def.palette = "unifieddyes_palette_extended.png"
+		new_def.groups.ud_param2_colorable = 1
+		new_def.on_dig = unifieddyes.on_dig
+	end
+
+	core.register_node("trampoline:" .. name, new_def)
 end
